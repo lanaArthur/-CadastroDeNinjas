256773694ff6e6c64bea576c80d6f3c848238f3c
lanaArthur <lanaarthur5@gmail.com>
2026-08-13 06:16:45 -0300

    Git amend

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,24 @@
+package dev.java10x.CadastroDeNinjas;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+
+    public NinjaModel() {}
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+}
